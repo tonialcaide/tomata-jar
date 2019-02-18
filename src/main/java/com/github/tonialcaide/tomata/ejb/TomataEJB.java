6a4dcb34599ffadd4f9807c4cab3b846b154d8bb
Alcaide Navarro, Antonio <aalcaide@umivale.es>
2019-02-18 09:10:31 +0100

    Initial commit

---
+++ src/main/java/com/github/tonialcaide/tomata/ejb/TomataEJB.java
@@ -0,0 +1,12 @@
+package com.github.tonialcaide.tomata.ejb;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class TomataEJB {
+
+    public String test() {
+        return "It is working!";
+    }
+
+}
